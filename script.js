e47a6d8103045db61a3428955edbbe8693d96af1
chaimae <chaimaemehray129@gmail.com>
2024-11-21 16:09:38 +0100

    Ajout des fichiers du site web

---
+++ script.js
@@ -0,0 +1,22 @@
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+// Change text and gif when the Yes button is clicked
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = " mais moi je t'aime plus que toi 🤭🩷";
+  gif.src = "https://media.giphy.com/media/W6XA4lxY1DoEEdMEtx/giphy.gif";
+});
+// Make the No button move randomly on hover
+noBtn.addEventListener("mouseover", () => {
+  const wrapper = document.querySelector(".wrapper");
+  const wrapperRect = wrapper.getBoundingClientRect();
+  const noBtnRect = noBtn.getBoundingClientRect();
+  // Calculate max positions to ensure the button stays within the wrapper
+  const maxX = wrapperRect.width - noBtnRect.width;
+  const maxY = wrapperRect.height - noBtnRect.height;
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
